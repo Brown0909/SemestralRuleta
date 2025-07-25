37f8475d060912c13d407ab5534266130e7ae094
mfniebles <monserrate.niebles@utp.ac.pa>
2025-07-24 20:41:51 -0500

    Creación de la clase apuestas

---
+++ src/main/java/com/brown0909/semestralruleta/Apuesta.java
@@ -0,0 +1,36 @@
+package com.brown0909.final_programacion;
+
+
+public class Apuesta {
+
+    private String tipoApuesta;
+    private double monto;
+    private int numeroApostado;
+
+    public Apuesta(String tipoApuesta, double monto) {
+        this.tipoApuesta = tipoApuesta;
+        this.monto = monto;
+        this.numeroApostado = -1;
+    }
+
+
+    public String getTipoApuesta() {
+        return tipoApuesta;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public int getNumeroApostado() {
+        return numeroApostado;
+    }
+
+
+    public void setNumeroApostado(int numeroApostado) {
+        this.numeroApostado = numeroApostado;
+    }
+
+    public void setTipoApuesta(String numero) {
+    }
+}
