9ca8e66b4abf0a2be381337fe605046f90a15e51
mfniebles <monserrate.niebles@utp.ac.pa>
2025-07-24 21:23:47 -0500

    Creación de la clase para el jugador

---
+++ src/main/java/com/brown0909/semestralruleta/Jugador.java
@@ -0,0 +1,52 @@
+package com.brown0909.semestralruleta;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Jugador {
+
+    private String nombreJugador;
+    private double saldo;
+    private List<com.brown0909.final_programacion.Apuesta> apuestasActuales;
+
+    public Jugador(String nombreJugador, double saldoInicial) {
+        this.nombreJugador = nombreJugador;
+        this.saldo = saldoInicial;
+        this.apuestasActuales = new ArrayList<>();
+    }
+
+
+    public boolean realizarApuesta(com.brown0909.final_programacion.Apuesta apuesta) {
+        if (apuesta.getMonto() > this.saldo) {
+            System.out.println("Saldo insuficiente para realizar la apuesta.");
+            return false;
+        }
+        this.saldo -= apuesta.getMonto();
+        this.apuestasActuales.add(apuesta);
+        System.out.println(nombreJugador + " ha apostado $" + apuesta.getMonto() + " a " + apuesta.getTipoApuesta());
+        return true;
+    }
+
+    public void recibirGanancia(double cantidad) {
+        this.saldo += cantidad;
+    }
+
+    public void limpiarApuestas() {
+        this.apuestasActuales.clear();
+    }
+
+
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public List<com.brown0909.final_programacion.Apuesta> getApuestasActuales() {
+        return apuestasActuales;
+    }
+}
